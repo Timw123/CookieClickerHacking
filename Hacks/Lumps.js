a60fc3f079b1215ae2504b687fe887e2da6061a6
Jade <44099468+JadeGrey@users.noreply.github.com>
2021-09-08 17:55:02 -0500

    Create Lumps.js

---
+++ Hacks/Lumps.js
@@ -0,0 +1,6 @@
+//inspect element
+var lumpsSelect = prompt("Any positive number")
+Game.lumps=lumpsSelect
+
+//bookmarklet
+javascript:(function()%7Bvar%20lumpsSelect%20%3D%20prompt(%22Any%20positive%20number%22)%0AGame.lumps%3DlumpsSelect%7D)()%3B
