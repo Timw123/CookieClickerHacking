be4b5bec3e907fa9bb9078c0e0fafcab0aada628
Jade <44099468+JadeGrey@users.noreply.github.com>
2021-09-08 17:59:15 -0500

    Update Cookies.js

--- Hacks/Cookies.js
+++ Hacks/Cookies.js
@@ -1,6 +1,11 @@
+/*
+NOTICE:
+Do NOT type a letter or a 0 before the select amount!
+*/
+
 //inspect element
-var cookieValueHack = prompt("Any positive number");
-Game.Earn(cookieValueHack);
+var cookieAdd = prompt("Any positive number");
+Game.Earn(cookieAdd);
 
 //bookmarklet code
-javascript:(function()%7Bvar%20cookieValueHack%20%3D%20prompt(%22Any%20positive%20number%22)%3B%0AGame.Earn(cookieValueHack)%3B%7D)()%3B
+javascript:(function()%7Bvar%20cookieAdd%20%3D%20prompt(%22Any%20positive%20number%22)%3B%0AGame.Earn(cookieAdd)%3B%7D)()%3B
