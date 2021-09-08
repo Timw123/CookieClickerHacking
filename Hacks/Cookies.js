b29da36748a54a71a0382c92eb0306cd2a019124
Jade <44099468+JadeGrey@users.noreply.github.com>
2021-09-08 17:21:52 -0500

    Create Cookies.js

---
+++ Hacks/Cookies.js
@@ -0,0 +1,6 @@
+//inspect element
+var cookieValueHack = prompt("Any positive number");
+Game.Earn(cookieValueHack);
+
+//bookmarklet code
+javascript:(function()%7Bvar%20cookieValueHack%20%3D%20prompt(%22Any%20positive%20number%22)%3B%0AGame.Earn(cookieValueHack)%3B%7D)()%3B
